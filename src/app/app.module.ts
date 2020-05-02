313ee6aa274314ca14fe342d6b5106842ed8890a
jdiaz <jdiaz@tiqal.com>
2020-05-01 22:59:24 -0500

    creación de cabecera y lista de recursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimelineComponent
+    TimelineComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -7,9 +7,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
+  public units: Units[];
   constructor() { }
 
   ngOnInit() {
+
+    this.units = [
+      {
+        name: 'Listening',
+        icon: 'ear.svg',
+        type: 'listening',
+        percentage: 75,
+        themes: [
+          {
+            name: 'Understand personal information questions',
+            state: States.completed
+          },
+          {
+            name: 'Understand greetings and farewells',
+            state: States.completed
+          },
+          {
+            name: 'Understand spelled words',
+            state: States.in_progress
+          },
+          {
+            name: 'Identify family members, occupations and characteristics.',
+            state: States.awaiting
+          }
+        ]
+      },
+      {
+        name: 'Speaking',
+        icon: 'chat.svg',
+        type: 'speaking',
+        percentage: 23,
+        themes: [
+          {
+            name: 'Recite the alphabet',
+            state: States.completed
+          },
+          {
+            name: 'Ask and give the time',
+            state: States.awaiting
+          },
+          {
+            name: 'Introduce oneself',
+            state: States.awaiting
+          },
+          {
+            name: 'Greet people and say goodbye',
+            state: States.awaiting
+          },
+          {
+            name: 'Ask and give basic information',
+            state: States.awaiting
+          },
+          {
+            name: 'Talk about peoples’ nationality',
+            state: States.awaiting
+          }
+
+        ]
+      },
+      {
+        name: 'Lenguage',
+        icon: 'translate.svg',
+        type: 'lenguage',
+        percentage: 0,
+        themes: [
+          {
+            name: 'Verb to Be. Af rmative, negative and interrogative',
+            state: States.awaiting
+          },
+          {
+            name: 'Prepositions of time',
+            state: States.awaiting
+          },
+          {
+            name: 'Wh- questions.WRITING',
+            state: States.awaiting
+          }
+        ]
+      }
+    ];
   }
 
+}
+
+export interface Units {
+  name: string;
+  type: string;
+  icon: string;
+  themes: Themes[];
+  percentage: number;
+}
+
+export interface Themes {
+  name: string;
+  state: States;
+}
+
+export enum States {
+  awaiting,
+  in_progress,
+  completed
 }
