99eee4417d966a36bfdb456802f32820c6c412fc
joseochoa227 <joseochoa227@gmail.com>
2020-08-03 09:30:10 -0500

    Agregado foto de perfil

--- src/app/student-module/profile/profile.component.ts
+++ src/app/student-module/profile/profile.component.ts
@@ -10,21 +10,22 @@ export class ProfileComponent implements OnInit {
   public name: string;
   public id: string;
   public cursoName: string;
+  public photo: string;
 
   constructor(private router: Router) {
     this.name = "Lorena Torres";
     this.id = "93248497";
     this.cursoName = "EASY ENGLISH";
-  }
-
-  ngOnInit(): void {
     this.getUser();
   }
+
+  ngOnInit(): void {}
 
   getUser() {
     let user = JSON.parse(localStorage.getItem("currentUser"));
     console.log(user);
-    /* this.name = user.name;
-    this.id = user.id; */
+    this.name = user.user.name;
+    this.id = user.user.id;
+    this.photo = user.user.photo;
   }
 }
